66cc8cf1dc5a6932c55fc1cb5eaeb2c8d344607c
Manuel-Lauriola <manuelauriol@gmail.com>
2025-09-03 18:20:17 +0200

    [UPD] - implement the logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react"
 
 function App() {
+
+  const [ formData, setFormData ] = useState({
+    title: "",
+    author: "",
+    post: "",
+    isPublic: false
+  })
+
+  function handleFormData(e) {
+
+    const value =
+      e.target.type === "checkbox"? e.target.checked:e.target.value
+
+    setFormData((formData) => ({
+      ...formData, [e.target.name]:value
+    }))
+  }
+
   return (
     <>
       <div className="container">
@@ -10,24 +29,35 @@ function App() {
                 <div className="col-4">
                   <input type="text"
                     name="title"
+                    value={formData.title}
+                    onChange={handleFormData}
                     placeholder="titolo"
                     className="form-control title" />
                 </div>
                 <div className="col-4">
                   <input type="text"
                     name="author"
+                    value={formData.author}
+                    onChange={handleFormData}
                     placeholder="autore"
                     className="form-control author" />
                 </div>
                 <div className="col-4">
-                  <input type="checkbox"
+                  <div className="form-check">
+                    <input type="checkbox"
                     name="isPublic"
-                    className="isPublic form-check-inline" />
-                  <label htmlFor="isPublic">Pubblico</label>
+                    id="isPublic"
+                    checked={formData.isPublic}
+                    onChange={handleFormData}
+                    className="isPublic form-check-input" />
+                    <label htmlFor="isPublic" className="form-check-label">Pubblico</label>
+                  </div>
                 </div>
                 <div className="col-12">
                   <input type="text"
                     name="post"
+                    value={formData.post}
+                    onChange={handleFormData}
                     placeholder="scrivi il tuo post"
                     className="form-control post" />
                 </div>
